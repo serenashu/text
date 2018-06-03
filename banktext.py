114a9292c2aeadeffced69d3814c26e1324f58c5
Jiecao Wang <jiecaowang@inside-65-30-12.wireless.utoronto.ca>
2018-06-03 14:36:06 -0400

    d

--- banktext.py
+++ banktext.py
@@ -6,142 +6,138 @@ Created on Thu May 31 19:15:42 2018
 """
 
 # download SEC bank financial statements
-#Goes to the SEC file and automatically construct the dictionary, then we can extract the MD&A stuff
- 
+# Goes to the SEC file and automatically construct the dictionary, then we can extract the MD&A stuff
+
 import numpy as np
 import os
 import pandas as pd
 import requests
 import codecs
 from html.parser import HTMLParser
- 
-#This gets rid off all HTML tag lines
+
+
+# This gets rid off all HTML tag lines
 class MLStripper(HTMLParser):
     def __init__(self):
         self.reset()
         self.strict = False
-        self.convert_charrefs= True
+        self.convert_charrefs = True
         self.fed = []
+
     def handle_data(self, d):
         self.fed.append(d)
+
     def get_data(self):
         return ''.join(self.fed)
- 
+
+
 def strip_tags(html):
     s = MLStripper()
     s.feed(html)
     return s.get_data()
-  
+
+
 # Two output directories, one for the 10-Ks, one for the Edgar index. They
 # need to exist.
 save_dir = '10K/'
 index_dir = 'index/'
 path = 'E:/Researchcode/Banktext'  # path to where to save your files
- 
- 
+
 # create the directory on local machine, these will create two different folder names
-dir_ = (path+index_dir)
-savedir_ = (path+save_dir)
- 
-#This will create a directory called 10k and index
+dir_ = (path + index_dir)
+savedir_ = (path + save_dir)
+
+# This will create a directory called 10k and index
 if not os.path.exists(dir_):
     os.mkdir(dir_)
     os.mkdir(savedir_)
-     
+
 # os.mkdir:Create a directory named path with numeric mode mode.     
- 
- 
- 
+
+
 # First, download the index files. Start from 2016 and end in 2016
-start_year = 1993 # Min 1993
-start_quarter = 1 # (1-4)
+start_year = 1993  # Min 1993
+start_quarter = 1  # (1-4)
 end_year = 2017
 end_quarter = 4
- 
- 
- 
+
 # Generate the list of quarterly index files to download.
 # Creates a list of tuples, so you have a list under the name qtr_list and inside
 # it will have (2016,Q1) etc 
- 
-qtr_list1 = [(start_year, q+1) for q in range(start_quarter-1, 4)]
-qtr_list2 = [(y, q+1) for y in range(start_year + 1, end_year)
+
+qtr_list1 = [(start_year, q + 1) for q in range(start_quarter - 1, 4)]
+qtr_list2 = [(y, q + 1) for y in range(start_year + 1, end_year)
              for q in range(4)]
-qtr_list3 = [(end_year, q+1) for q in range(0, end_quarter)]
+qtr_list3 = [(end_year, q + 1) for q in range(0, end_quarter)]
 qtr_list = qtr_list1 + qtr_list2 + qtr_list3
- 
-print (qtr_list1)
-print (qtr_list2)
-print (qtr_list3)
-print (qtr_list)
- 
+
+print(qtr_list1)
+print(qtr_list2)
+print(qtr_list3)
+print(qtr_list)
+
 # Download all the quarterly index files. 
 # Now it will loop over the tuples, y is the year, and q is the quarter 
 # So it will automatically download each quarterly-yearly master file
- 
+
 for y, q in qtr_list:
-    url = ('https://www.sec.gov/Archives/edgar/full-index/' + str(y) 
+    url = ('https://www.sec.gov/Archives/edgar/full-index/' + str(y)
            + '/QTR' + str(q) + '/master.gz')
     print('Downloading ' + str(y) + '-Q' + str(q) + ' ...')
     with open(dir_ + 'master_' + str(y) + '-Q' + str(q) + '.gz', 'wb') as f:
         f.write(requests.get(url).content)
     print('Done')
-     
-     
- 
+
 # Load all index files in pandas.
 # Now loop over these files and use pandas.
 # If you unzip these files, you get txt files. So you need to use a delimiter 
 # Skip the 11 lines since they are copyright stuff
 # Then it gets the names, CIK etc
 # Here are all of the companies and their names    
-     
+
 idx_list = [x for x in os.listdir(dir_) if not x.startswith('.')]
 dfs = []
-print (idx_list)
-print (dir_)
+print(idx_list)
+print(dir_)
 import csv
-df = pd.read_csv('E:/Researchcode/Banktextindex/master_1993-Q1.gz',delimiter='|',
-                     names=['CIK', 'Company Name', 'Form Type',
-                            'Date Filed', 'Filename'],
-                     skiprows=11)
-print (df)
-help (pd.read_csv)
+
+df = pd.read_csv('E:/Researchcode/Banktextindex/master_1993-Q1.gz', delimiter='|',
+                 names=['CIK', 'Company Name', 'Form Type',
+                        'Date Filed', 'Filename'],
+                 skiprows=11)
+print(df)
+help(pd.read_csv)
 
 for x in idx_list:
     df = pd.read_csv(dir_ + x, delimiter='|',
                      names=['CIK', 'Company Name', 'Form Type',
-                            'Date Filed', 'Filename'],encoding='latin-1',
+                            'Date Filed', 'Filename'], encoding='latin-1',
                      skiprows=11)
     dfs.append(df)
-print (df)
+print(df)
 master = pd.concat(dfs)
-print (master)
-#Click on 'master' then just run this part by right clickign to see the form types
+print(master)
+# Click on 'master' then just run this part by right clickign to see the form types
 # dfs = [] is a list and then you just concatinate 
- 
- 
- 
+
+
 # Extract only 10-Ks, 
 master_10k = master[master['Form Type'] == '10-K']
 master_10q = master[master['Form Type'] == '10-Q']
-print (master_10k)
-
+print(master_10k)
 
 # Extract bank 10-Q
 comp = pd.read_csv('E:/Researchcode/Banktext/Bankcik.csv')
 master_10q_bank = master_10q[master_10q['CIK'].isin(comp['CIK'])]
-master_10q_bank.sort_values(by=['CIK','Date Filed'])
+master_10q_bank.sort_values(by=['CIK', 'Date Filed'])
 
 master_10k_bank = master_10k[master_10k['CIK'].isin(comp['CIK'])]
- 
- 
+
 # Get the Filename (includes part of the path.) If you have more file names for a given 
 # company, it will loop over these files 
- 
+
 fn_list = [x for x in master_10k_bank['Filename']]
- 
- 
+
 # Download all the files from EDGAR 
 for fn in fn_list:
     url = 'https://www.sec.gov/Archives/' + fn
@@ -149,85 +145,79 @@ for fn in fn_list:
     with open(savedir_ + fn.split('/')[-1], 'wb') as f:
         f.write(requests.get(url).content)
     print('Done')
-      
-#%%dictionary
+
+# %%dictionary
 url = 'https://www3.nd.edu/~mcdonald/Word_Lists_files/LoughranMcDonald_MasterDictionary_2014.xlsx'
- 
+
 lm = pd.read_excel(url)
-lmpos = list(lm[lm.Positive!=0]['Word'])
-lmneg = list(lm[lm.Negative!=0]['Word'])
-lmunc = list(lm[lm.Uncertainty!=0]['Word'])
- 
+lmpos = list(lm[lm.Positive != 0]['Word'])
+lmneg = list(lm[lm.Negative != 0]['Word'])
+lmunc = list(lm[lm.Uncertainty != 0]['Word'])
+
 del mda
- 
-#%% Extract the MD&A section
+
+# %% Extract the MD&A section
 savedir_ = 'E:/Researchcode/Banktext/Banktext10K'
 files = os.listdir(savedir_)
 for f in files:
-    with codecs.open(savedir_+f,'rb',encoding='latin-1') as fin:
+    with codecs.open(savedir_ + f, 'rb', encoding='latin-1') as fin:
         text = fin.read()
-     
-#Since the MDA section is very constant, you can select everything that is between
-# the ITEM7s and ITEM8s. If he doesn't find it, it will say -1 
-# All information between items that will start  with ITEM 7 and end with ITEM 8    
- 
+
+        # Since the MDA section is very constant, you can select everything that is between
+        # the ITEM7s and ITEM8s. If he doesn't find it, it will say -1
+        # All information between items that will start  with ITEM 7 and end with ITEM 8
+
         item7_begins = ['ITEM 7.', 'ITEM 7 –', 'ITEM 7:', 'ITEM 7 ',
-                'Item 7.', 'Item 7 –', 'Item 7:', 'Item 7 ']
-        item7_ends   = ['ITEM 8', 'Item 8', 'Item 8.']
- 
+                        'Item 7.', 'Item 7 –', 'Item 7:', 'Item 7 ']
+        item7_ends = ['ITEM 8', 'Item 8', 'Item 8.']
+
         beg = []
         for tx in item7_begins:
             if text.find(tx) != -1:
                 beg.append(text.rfind(tx))
- 
+
         end = []
         for tx in item7_ends:
             if text.find(tx) != -1:
                 end.append(text.rfind(tx))
- 
-    
-#Text is where the MDA is. .STRIP() gets rid off the extra spaces
+
+        # Text is where the MDA is. .STRIP() gets rid off the extra spaces
         mda = text[beg[0]:end[0]].strip()
         mda = strip_tags(mda)
- 
-# remove numbers
+
+        # remove numbers
         mda = ''.join([i for i in mda if not i.isdigit()])
- 
-# remove unwanted characters
+
+        # remove unwanted characters
         for notwanted in [',,', '$', ':', ',', '.', '%', ';', '|',
-                  '\xa0', '&gt', '&apos', '&amp', '&quot',
-                  'Form -K', '\\', '0x92']:
+                          '\xa0', '&gt', '&apos', '&amp', '&quot',
+                          'Form -K', '\\', '0x92']:
             mda = mda.replace(notwanted, ' ')
- 
-# Capitalize because words in LM are in CAPs
+
+        # Capitalize because words in LM are in CAPs
         mda = mda.upper()
- 
-# Convert string into list
+
+        # Convert string into list
         mda = mda.split()
         df_mda = pd.DataFrame(mda, columns=["words"])
-        df_mda.to_csv(savedir_+f+'cleaned.csv', index=False)
- 
+        df_mda.to_csv(savedir_ + f + 'cleaned.csv', index=False)
+
 # load LM dictionnary
 # get the Loughran McDonald MasterDictionary at 
- 
-#%%
+
+# %%
 nwords = len(mda)
 npos = len([i for i in mda if i in lmpos])
 nneg = len([i for i in mda if i in lmneg])
 nunc = len([i for i in mda if i in lmunc])
- 
- 
+
 """ NLTK - Really important package in textual analysis """
 import nltk
+
 # http://www.nltk.org/
 # http://www.nltk.org/book/ch01.html
- 
+
 tokens = [t for t in mda]
-freq= nltk.FreqDist(tokens)
-for key,val in freq.items(): 
-    print (str(key) + ':' + str(val))
-
-
-
-
-
+freq = nltk.FreqDist(tokens)
+for key, val in freq.items():
+    print(str(key) + ':' + str(val))
--- banktext6.3.py
+++ banktext6.3.py
@@ -1,241 +1,173 @@
-
-
-import numpy as np
-import os
-import pandas as pd
-import requests
 import codecs
-from html.parser import HTMLParser
-import csv
-import re
-import nltk
-import glob
-#This gets rid off all HTML tag lines
-class MLStripper(HTMLParser):
-    def __init__(self):
-        self.reset()
-        self.strict = False
-        self.convert_charrefs= True
-        self.fed = []
-    def handle_data(self, d):
-        self.fed.append(d)
-    def get_data(self):
-        return ''.join(self.fed)
- 
-def strip_tags(html):
-    s = MLStripper()
-    s.feed(html)
-    return s.get_data()
-
-
-import glob
-savedir_ = 'E:/Researchcode/Banktext10Q/'
-files = os.listdir(savedir_)
-
-corpus = []
-for f in files:  
-    with codecs.open(savedir_+f,'rb',encoding='latin-1') as fin:
-        corpus.append(fin.read())
-
-#%% Extract the MD&A section
-savedir_ = 'E:/Researchcode/Banktext10Q/'
-cleandir_ = 'E:/Researchcode/Banktext10QClean'
-files = os.listdir(savedir_)
-print (files)
-for f in files:
-    with codecs.open(savedir_+f,'rb',encoding='latin-1') as fin:
-        text = fin.read()
-     
-#Since the MDA section is very constant, you can select everything that is between
-# the ITEM7s and ITEM8s. If he doesn't find it, it will say -1 
-# All information between items that will start  with ITEM 7 and end with ITEM 8    
- 
-        item2_begins = ['ITEM 2.', 'ITEM 2 –', 'ITEM 2:', 'ITEM 2 ',
-                'Item 2.', 'Item 2 –', 'Item 2:', 'Item 2 ']
-        item2_ends   = ['ITEM 3', 'Item 3', 'ITEM 3.', 'Item 3.']
- 
-        beg = []
-        for tx in item2_begins:
-            if text.find(tx) != -1:
-                beg.append(text.rfind(tx))
- 
-        end = []
-        for tx in item2_ends:
-            if text.find(tx) != -1:
-                end.append(text.rfind(tx))
- 
-    
-#Text is where the MDA is. .STRIP() gets rid off the extra spaces
-        mda = text[beg[0]:end[0]].strip()
-        mda = strip_tags(mda)
-
-  
-# remove numbers
-        mda = ''.join([i for i in mda if not i.isdigit()])
-    
-# remove unwanted characters
-# remove unwanted characters
-        for notwanted in [',,', '$', ':', ',', '.', '%', ';', '|',
-                  '\xa0', '&gt', '&apos', '&amp', '&quot',
-                  'Form -K', '\\', '0x92','*','"','---','(',')','=','-','/','======','...','&']:
-            mda = mda.replace(notwanted, ' ')  
-    
-# remove white space and new lines
-        mda = re.sub('\s+',' ',mda)  
-      
- # Capitalize because words in LM are in CAPs
-        mda = mda.upper()
- 
-# Convert string into list
-        mda = mda.split()
-        df_mda = pd.DataFrame(mda, columns=["words"])
-        df_mda.to_csv(savedir_+f+'cleaned.csv', index=False)
-        
-    
-#gensim  
-    
-import logging  
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)    
+import logging
 import os
+import re
 import tempfile
-TEMP_FOLDER = tempfile.gettempdir()
-print('Folder "{}" will be used to save temporary dictionary and corpus.'.format(TEMP_FOLDER))
-
-
-
-from gensim import corpora    
-
-documents  = ["Human machine interface for lab abc computer applications applications",
-             "A survey of user opinion of computer system response time",
-             "The EPS user interface management system",
-             "System and human system engineering testing of EPS",              
-             "Relation of user perceived response time to error measurement",
-             "The generation of random binary unordered trees",
-             "The intersection graph of paths in trees",
-             "Graph minors IV Widths of trees and well quasi ordering",
-             "Graph minors A survey"]    
-
-# remove common words and tokenize
-stoplist = set('for a of the and to in'.split())
-texts = [[word for word in document.lower().split() if word not in stoplist]
-         for document in documents]    
-print (texts)   
-# remove words that appear only once
-from collections import defaultdict
-frequency = defaultdict(int)
-for text in texts:
-    for token in text:
-        frequency[token] += 1
-
-texts = [[token for token in text if frequency[token] > 1] for text in texts]
-print (texts)
+from html.parser import HTMLParser
 from pprint import pprint  # pretty-printer
-pprint(texts)   
-    
-from pprint import pprint  # pretty-printer
-pprint(texts)    
-    
-dictionary = corpora.Dictionary(texts)
-dictionary.save(os.path.join(TEMP_FOLDER, 'deerwester.dict'))  # store the dictionary, for future reference
-print(dictionary)   
 
-print(dictionary.token2id)   
-    
-
-new_doc = "Human computer interaction"
-new_vec = dictionary.doc2bow(new_doc.lower().split())
-print(new_vec)  # the word "interaction" does not appear in the dictionary and is ignored    
-    
-    
-corpus = [dictionary.doc2bow(text) for text in texts]
-corpora.MmCorpus.serialize(os.path.join(TEMP_FOLDER, 'deerwester.mm'), corpus)  # store to disk, for later use
-for c in corpus:
-    print(c)   
-    
-    
-    
+import pandas as pd
+from six import iteritems
 from smart_open import smart_open
+from gensim import corpora
+from collections import defaultdict
+
+
 class MyCorpus(object):
     def __iter__(self):
         for line in smart_open('E:/dataset/mycorpus.txt', 'rb'):
             # assume there's one document per line, tokens separated by whitespace
-            yield dictionary.doc2bow(line.lower().split())    
-    
-    
-corpus_memory_friendly = MyCorpus()    
-print(corpus_memory_friendly)    
-    
-for vector in corpus_memory_friendly:  # load one vector into memory at a time
-    print(vector)   
-    
-from six import iteritems
-from smart_open import smart_open    
-    
+            yield dictionary.doc2bow(line.lower().split())
 
-# collect statistics about all tokens
-dictionary = corpora.Dictionary(line.lower().split() for line in smart_open('E:/dataset/mycorpus.txt', 'rb'))
 
-# remove stop words and words that appear only once
-stop_ids = [dictionary.token2id[stopword] for stopword in stoplist 
-            if stopword in dictionary.token2id]
-once_ids = [tokenid for tokenid, docfreq in iteritems(dictionary.dfs) if docfreq == 1]
+# This gets rid off all HTML tag lines
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.fed = []
 
-# remove stop words and words that appear only once
-dictionary.filter_tokens(stop_ids + once_ids)
-print(dictionary)    
-    
-    
-    
- 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ''.join(self.fed)
+
+
+if __name__ == "__main__":
+    bank_text_dir = 'E:/Researchcode/Banktext10Q/'
+    bank_text_files = os.listdir(bank_text_dir)
+    latin_encoding = 'latin'
+
+    def strip_tags(html):
+        s = MLStripper()
+        s.feed(html)
+        return s.get_data()
+
+    corpus = {}
+
+    # for bank_text_file in bank_text_files:
+    #     with codecs.open(bank_text_dir + bank_text_file, 'rb', encoding=latin_encoding) as fin:
+    #         corpus.append(fin.read())
+
+    # %% Extract the MD&A section
+    bank_text_dir = 'E:/Researchcode/Banktext10Q/'
+    cleandir_ = 'E:/Researchcode/Banktext10QClean'
+    bank_text_files = os.listdir(bank_text_dir)
+    print(bank_text_files)
+    for bank_text_file in bank_text_files:
+        with codecs.open(bank_text_dir + bank_text_file, 'rb', encoding=('%s-1' % latin_encoding)) as fin:
+            text = fin.read()
+
+            # Since the MDA section is very constant, you can select everything that is between
+            # the ITEM7s and ITEM8s. If he doesn't find it, it will say -1
+            # All information between items that will start  with ITEM 7 and end with ITEM 8
+
+            item2_begins = ['ITEM 2.', 'ITEM 2 –', 'ITEM 2:', 'ITEM 2 ',
+                            'Item 2.', 'Item 2 –', 'Item 2:', 'Item 2 ']
+            item2_ends = ['ITEM 3', 'Item 3', 'ITEM 3.', 'Item 3.']
+
+            beg = []
+            for tx in item2_begins:
+                if text.find(tx) != -1:
+                    beg.append(text.rfind(tx))
+
+            end = []
+            for tx in item2_ends:
+                if text.find(tx) != -1:
+                    end.append(text.rfind(tx))
+
+            # Text is where the MDA is. .STRIP() gets rid off the extra spaces
+            mda = text[beg[0]:end[0]].strip()
+            mda = strip_tags(mda)
+
+            # remove numbers
+            mda = ''.join([i for i in mda if not i.isdigit()])
+
+            # remove unwanted characters
+            # remove unwanted characters
+            for notwanted in [',,', '$', ':', ',', '.', '%', ';', '|',
+                              '\xa0', '&gt', '&apos', '&amp', '&quot',
+                              'Form -K', '\\', '0x92', '*', '"', '---', '(', ')', '=', '-', '/', '======', '...', '&']:
+                mda = mda.replace(notwanted, ' ')
+
+            # remove white space and new lines
+            mda = re.sub('\s+', ' ', mda)
+
+            # Capitalize because words in LM are in CAPs
+            mda = mda.upper()
+            corpus[bank_text_file.name] = mda
+
+            # Convert string into list
+            df_mda.to_csv(bank_text_dir + bank_text_file + 'cleaned.csv', index=False)
+
+    # gensim
+
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    TEMP_FOLDER = tempfile.gettempdir()
+    print('Folder "{}" will be used to save temporary dictionary and corpus.'.format(TEMP_FOLDER))
+
+
+    documents = ["Human machine interface for lab abc computer applications applications",
+                 "A survey of user opinion of computer system response time",
+                 "The EPS user interface management system",
+                 "System and human system engineering testing of EPS",
+                 "Relation of user perceived response time to error measurement",
+                 "The generation of random binary unordered trees",
+                 "The intersection graph of paths in trees",
+                 "Graph minors IV Widths of trees and well quasi ordering",
+                 "Graph minors A survey"]
+
+    # remove common words and tokenize
+    stoplist = set('for a of the and to in'.split())
+    texts = [[word for word in document.lower().split() if word not in stoplist]
+             for document in documents]
+    print(texts)
+    # remove words that appear only once
+
+    frequency = defaultdict(int)
+    for text in texts:
+        for token in text:
+            frequency[token] += 1
+
+    texts = [[token for token in text if frequency[token] > 1] for text in texts]
+    print(texts)
+
+    pprint(texts)
+
+    pprint(texts)
+
+    dictionary = corpora.Dictionary(texts)
+    dictionary.save(os.path.join(TEMP_FOLDER, 'deerwester.dict'))  # store the dictionary, for future reference
+    print(dictionary)
+
+    print(dictionary.token2id)
+
+    new_doc = "Human computer interaction"
+    new_vec = dictionary.doc2bow(new_doc.lower().split())
+    print(new_vec)  # the word "interaction" does not appear in the dictionary and is ignored
+
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    corpora.MmCorpus.serialize(os.path.join(TEMP_FOLDER, 'deerwester.mm'), corpus)  # store to disk, for later use
+    for c in corpus:
+        print(c)
+
+    corpus_memory_friendly = MyCorpus()
+    print(corpus_memory_friendly)
+
+    for vector in corpus_memory_friendly:  # load one vector into memory at a time
+        print(vector)
+
+    # collect statistics about all tokens
+    dictionary = corpora.Dictionary(line.lower().split() for line in smart_open('E:/dataset/mycorpus.txt', 'rb'))
+
+    # remove stop words and words that appear only once
+    stop_ids = [dictionary.token2id[stopword] for stopword in stoplist
+                if stopword in dictionary.token2id]
+    once_ids = [tokenid for tokenid, docfreq in iteritems(dictionary.dfs) if docfreq == 1]
+
+    # remove stop words and words that appear only once
+    dictionary.filter_tokens(stop_ids + once_ids)
+    print(dictionary)
